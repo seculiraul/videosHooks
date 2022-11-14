9db9414616733380c12234b4b4ebfd54bcef963b
Raul Seculi <raulseculi@Rauls-MacBook-Pro-2.local>
2022-11-14 21:53:09 +0200

    S14L215

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,28 @@
 import React, { useEffect, useState } from "react";
 import SearchBar from "./SearchBar";
-import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
 import VideoDetail from "./VideoDetail";
+import useVideos from "../hooks/useVideos";
 
 
 const App = () => {
-    const [videos, setVideos] = useState([]);
     const [selectedVideo, setSelectedVideo] = useState(null);
+    const [videos, search] = useVideos('cars');
 
     useEffect(() => {
-        onTermSubmit('cars');
-    }, [])
-
-    const onTermSubmit = async(term) => {
-        const res = await youtube.get('/search', {
-              params: {
-                  q: term
-                }
-            })
-        setVideos(res.data.items);
-        setSelectedVideo(res.data.items[0]);
-    }
-
-    const onVideoSelect = video => {
-        setSelectedVideo(video);
-    }
+        setSelectedVideo(videos[0]);
+    }, [videos])
 
     return (
         <div className="ui container">
-                <SearchBar onFormSubmit={onTermSubmit}/>
+                <SearchBar onFormSubmit={search}/>
                 <div className="ui grid">
                     <div className="ui row">
                         <div className="eleven wide column">
                             <VideoDetail video={selectedVideo} />
                         </div>
                         <div className="five wide column">
-                            <VideoList videos={videos} onVideoSelect={onVideoSelect} />
+                            <VideoList videos={videos} onVideoSelect={setSelectedVideo} />
                         </div>
                     </div>
                 </div>
